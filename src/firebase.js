808c6e72821f17d275d7e475fcadbc53b83ab980
Sean <fuy0116@gmail.com>
2024-05-22 09:17:15 +0800

    +firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+ 
+
+
+// 初始化 Firestore 和 Auth
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
